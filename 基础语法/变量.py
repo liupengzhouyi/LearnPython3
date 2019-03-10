c6cdfee546a0447517a60fc815938f96f2f918d0
liupeng <LIUPENG.0@outlook.com>
2019-03-11 00:09:20 +0800

    基础语法.数学运算 V 1.0.1

---
+++ 基础语法/变量.py
@@ -0,0 +1,21 @@
+
+
+apple = "Iphone8 plus"
+
+print(apple)
+
+apple_2016 = "Ipone 7 plus mocbook"
+
+print(apple_2016)
+
+
+# 一次定义多个变量
+
+numberA, numberB, numberC = 1, 2, 3
+
+print(numberA)
+print(numberB)
+print(numberC)
+
+
+
