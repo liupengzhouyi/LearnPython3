400db59e02800e5cb3056013ee7de3fbd40f1548
liupeng <LIUPENG.0@outlook.com>
2019-03-11 00:18:11 +0800

    基础语法.循环.for V 1.0.2

---
+++ 基础语法/循环/for.py
@@ -0,0 +1,39 @@
+
+
+list = [1,2,3,4,5,6]
+
+for i in list:
+    print(i)
+
+print("----------------------------------")
+
+
+# range 函数
+
+listA = range(1,5)
+
+# 1, 2, 3, 4
+
+for number in listA:
+    print(number)
+
+print("----------------------------------")
+
+listB = range(3)
+
+# 0, 1, 2
+
+for number in listB:
+    print(number)
+
+print("----------------------------------")
+
+listC = range(0, 10, 2)
+
+# 步长
+# 0, 2, 4, 6, 8
+
+for number in listC:
+    print(number)
+
+
