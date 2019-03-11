120f035054b660eba44d2fa3d38ce5473a9ab45d
liupeng <LIUPENG.0@outlook.com>
2019-03-11 22:00:25 +0800

    基础语法.内置集合.dictionary V 1.0.0

---
+++ 基础语法/内置集合/dictionary.py
@@ -0,0 +1,13 @@
+
+
+dictionary = {}
+
+dictionary['001'] = 'phthon1.0'
+
+dictionary['002'] = 'python2.7'
+
+dictionary['003'] = 'python3.6'
+
+for key in dictionary:
+    print(key, dictionary[key])
+
