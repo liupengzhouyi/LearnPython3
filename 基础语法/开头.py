9c76cdec60739ab495ec4bf40850c5c8f267e7ef
liupeng <LIUPENG.0@outlook.com>
2019-03-12 23:48:21 +0800

    基础语法.数据类型.String V 1.0.2

--- 基础语法/开头.py
+++ 基础语法/开头.py
@@ -1,3 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
 
--- 基础语法/格式化.py
+++ 基础语法/格式化.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
 
 print('He! %s, you have $%d' % ('liupeng', 10000))
