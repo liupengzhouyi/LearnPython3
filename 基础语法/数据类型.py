1f431153c4fb5e3e55654a09836ebb8c5da6fb9a
liupeng <LIUPENG.0@outlook.com>
2019-03-12 23:36:05 +0800

    基础语法.数据类型 V 1.0.1

--- 基础语法/数据类型.py
+++ 基础语法/数据类型.py
@@ -1,2 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+number = 100                        # 整型变量
+miles = 100.98                      # 浮点型变量
+name01 = "liupeng01"                # 字符串 01
+name02 = 'liupeng02'                # 字符串 02
+name03 = """liupeng03"""            # 字符串 03
+myList = ['a', 123, '45', 12.45]    # 列表
+myTuple = ('a', 123, '45', 12.45)   # 元组
+mySet = {'a', 123, '45', 12.45}     # 集合
+myDictionary01 = {}                 # 字典
+myDictionary01['1'] = '111'
+myDictionary01['2'] = '222'
+myDictionary01['3'] = '333'
+
+myDictionary002 = {'01': 'my01', '02': 'my02', '03': 'my03'}    # 字典
+
+
+
+
+
+
+
 
 
