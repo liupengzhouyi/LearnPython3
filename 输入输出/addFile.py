01a65a7edc43b35eab61288bdb6a0c858029b3e5
liupeng <LIUPENG.0@outlook.com>
2019-04-11 10:45:52 +0800

    基础语法.输入输出.追加文件 V 1.0.1

---
+++ 输入输出/addFile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.7
+# -*- coding: UTF-8 -*-
+
+f = open('File/text.txt', 'a')
+
+f.write('\n')
+
+f.write('asdf12344')
+
+f.write('\n')
+
+
+f.close()
--- 输入输出/openFile.py
+++ 输入输出/openFile.py
@@ -7,17 +7,21 @@ txt = '''\
     if you wanna make your also fun:
         use Python!
 '''
-
+# 打开文件
 f = open('File/text.txt')
 
 while True:
     line = f.readline()
-
+    # 如果都到一行，而且这一行的长度为0
     if (line) == 0:
         break
-
+    # 打印这一行的内容到屏幕上
+    # 为什么需要有一个 end='' ,这是因为我们需要在这一样最后输出
+    # 但是，我们的在读取的过程中已经有了一个换行符
+    # 所以我们什么也不写入
     print(line, end='')
 
+# 关闭文件
 f.close()
 
 
