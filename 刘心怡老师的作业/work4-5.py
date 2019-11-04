9bb5ef97aecedde7b8938e6f3fd6f63267e71236
liupeng <liupeng.0@outlook.com>
2019-11-04 22:44:22 +0800

    刘心怡老师的作业4.5

---
+++ 刘心怡老师的作业/work4-5.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+# -*- author: liupeng -*-
+# -*- fileName: work4-5.py -*-
+# -*- data: 2019-11-04 -*-
+
+n = int(input())
+
+if n%2==0:
+    print(int(0-n/2))
+else:
+    print(1+int(n/2))
