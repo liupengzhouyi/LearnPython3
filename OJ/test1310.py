dc30a5721ad17105a8502958c8501ebf845ce7dc
liupeng <liupeng.0@outlook.com>
2019-11-06 16:29:38 +0800

    OJ 1310

---
+++ OJ/test1310.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# -*- author: liupeng -*-
+# -*- fileName: test1310.py -*-
+# -*- data: 2019-11-06 -*-
+# -*- url: https://imustacm.cn/problem/getProblem/1310
+import datetime
+
+date1 = datetime.datetime(2014, 11, 9)
+date2 = date1 + datetime.timedelta(days=1000)
+print(date2.date())
+
+
