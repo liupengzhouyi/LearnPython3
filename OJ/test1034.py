a2b92469484d8b04c625e99ce40b608ac5da8041
liupeng <liupeng.0@outlook.com>
2019-11-07 23:59:21 +0800

    OJ 1034

---
+++ OJ/test1034.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+# -*- author: liupeng -*-
+# -*- fileName: test1034.py -*-
+# -*- data: 2019-11-07 -*-
+
+
+n = int(input())
+while n > 0:
+    n = n - 1
+    a = str(input())
+    print(a[::-1])
