b99c8ed3434a35f59e62c080faffe4e98ffff36b
liupeng <liupeng.0@outlook.com>
2019-12-06 12:28:34 +0800

    刘心怡老师的作业5.4

---
+++ 刘心怡老师的作业/work5-4.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# -*- author: liupeng -*-
+# -*- fileName: work5-3.py -*-
+# -*- data: 2019-12-06 -*-
+
+listA = list(input().split(','))
+listA.append('红烧肉')
+listA.remove('水煮干丝')
+print(listA)
