4ce88f95924bfa0faaf6337f609dd74e5472ab9a
liupeng <liupeng.0@outlook.com>
2020-02-16 23:36:57 +0800

    Python.function.海🐢绘图 V 2.1.5

---
+++ 图新界面/dome02.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+width(4)
+
+# forword(200)
+
+
+pencolor('red')
+forward(50)
+
+right(90)
+pencolor('red')
+forward(50)
+
+right(90)
+pencolor('red')
+forward(100)
+
+right(90)
+pencolor('red')
+forward(100)
+
+right(90)
+pencolor('red')
+forward(100)
+
+right(90)
+pencolor('red')
+forward(50)
+
+done()
