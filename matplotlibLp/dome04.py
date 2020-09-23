73ef709858fab605e6c84634b1479a43601a8856
liupeng <liupeng.0@outlook.com>
2020-09-23 10:36:03 +0800

    pandas-III V 2.2.0

--- matplotlibLp/dome04.py
+++ matplotlibLp/dome04.py
@@ -9,15 +9,18 @@ x = np.arange(0,  3  * np.pi,  0.1)
 y_sin = np.sin(x)
 y_cos = np.cos(x)
 # 建立 subplot 网格，高为 2，宽为 1
+
 # 激活第一个 subplot
 plt.subplot(3,  1,  1)
 # 绘制第一个图像
 plt.plot(x, y_sin)
 plt.title('Sine')
+
 # 将第二个 subplot 激活，并绘制第二个图像
 plt.subplot(3,  1,  2)
 plt.plot(x, y_cos)
 plt.title('Cosine')
+
 # 将第三个 subplot 激活，并绘制第三个图像
 plt.subplot(3, 1, 3)
 x =  [5,8,10]
@@ -30,6 +33,7 @@ plt.bar(x2, y2, align =  'center')
 plt.title('Bar graph')
 plt.ylabel('Y axis')
 plt.xlabel('X axis')
+
 plt.savefig('image001.png')
 # 展示图像
 plt.show()
