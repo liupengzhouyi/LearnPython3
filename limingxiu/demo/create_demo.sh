8b5e38540b2bea27a007e7e2947e8b9eb0692e76
liupeng <liupeng.0@outlook.com>
2022-09-18 09:09:30 +0800

    add batch resize

--- limingxiu/demo/create_demo.sh
+++ limingxiu/demo/create_demo.sh
@@ -20,3 +20,38 @@ touch ./target_images/akb47/style_003.png
 touch ./target_images/akb48/style_001.png
 touch ./target_images/akb48/style_002.png
 touch ./target_images/akb48/style_003.png
+
+
+
+
+
+
+# rm -f ./target_images/akb45/style_001.png
+# rm -f ./target_images/akb45/style_002.png
+# rm -f ./target_images/akb45/style_003.png
+# rm -f ./target_images/akb46/style_001.png
+# rm -f ./target_images/akb46/style_002.png
+# rm -f ./target_images/akb46/style_003.png
+# rm -f ./target_images/akb47/style_001.png
+# rm -f ./target_images/akb47/style_002.png
+# rm -f ./target_images/akb47/style_003.png
+# rm -f ./target_images/akb48/style_001.png
+# rm -f ./target_images/akb48/style_002.png
+# rm -f ./target_images/akb48/style_003.png
+
+
+
+
+
+# cp ./target_images/akb45/image.png ./target_images/akb45/style_001.png
+# cp ./target_images/akb45/image.png ./target_images/akb45/style_002.png
+# cp ./target_images/akb45/image.png ./target_images/akb45/style_003.png
+# cp ./target_images/akb45/image.png ./target_images/akb46/style_001.png
+# cp ./target_images/akb45/image.png ./target_images/akb46/style_002.png
+# cp ./target_images/akb45/image.png ./target_images/akb46/style_003.png
+# cp ./target_images/akb45/image.png ./target_images/akb47/style_001.png
+# cp ./target_images/akb45/image.png ./target_images/akb47/style_002.png
+# cp ./target_images/akb45/image.png ./target_images/akb47/style_003.png
+# cp ./target_images/akb45/image.png ./target_images/akb48/style_001.png
+# cp ./target_images/akb45/image.png ./target_images/akb48/style_002.png
+# cp ./target_images/akb45/image.png ./target_images/akb48/style_003.png
